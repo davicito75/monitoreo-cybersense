f056bfebf7b367e4432968bab7855c660b33cde0
Davicito75 <davicito75@gmail.com>
2026-02-01 16:31:18 -0300

    fix: enable trust proxy for express rate limit on vercel

--- server/src/app.ts
+++ server/src/app.ts
@@ -21,6 +21,7 @@ import expressStatic from 'express';
 dotenv.config();
 
 const app = express();
+app.set('trust proxy', 1); // Enable for Vercel/Proxy environments
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
